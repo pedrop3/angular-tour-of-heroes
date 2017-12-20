eea77a4de79eac3cb5682cc8aa0c29326f82b7e7
pedro <pedrohenrique.acc1@gmail.com>
2017-12-19 23:35:22 -0200

    Component heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
